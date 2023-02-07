cc12c0ebe45f8cfc137bb226705fef11418767d7
Abuzarhaider <abuzarsarwary3@gmail.com>
2023-02-08 03:42:02 +0500

    Updates

---
+++ precache-manifest.674767699ccb0d1b34bdf3da27722057.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc83058d278c2eb94d24",
+    "url": "/ReactFirst-sCodeResume/static/css/main.8062c9a9.chunk.css"
+  },
+  {
+    "revision": "bc83058d278c2eb94d24",
+    "url": "/ReactFirst-sCodeResume/static/js/main.bc83058d.chunk.js"
+  },
+  {
+    "revision": "b965d220e0d6421fd4e0",
+    "url": "/ReactFirst-sCodeResume/static/js/1.b965d220.chunk.js"
+  },
+  {
+    "revision": "ac4bdd84289c8577c8a5",
+    "url": "/ReactFirst-sCodeResume/static/js/2.ac4bdd84.chunk.js"
+  },
+  {
+    "revision": "e84001a9431c58824158",
+    "url": "/ReactFirst-sCodeResume/static/js/runtime~main.e84001a9.js"
+  },
+  {
+    "revision": "7315fdf987caf84db86c8b3137b6284a",
+    "url": "/ReactFirst-sCodeResume/static/media/sampleImg.7315fdf9.png"
+  },
+  {
+    "revision": "5293c91aac1941e5d64927d6dab2f7f9",
+    "url": "/ReactFirst-sCodeResume/static/media/logo.5293c91a.png"
+  },
+  {
+    "revision": "6f9a2f99f101fb4240d08acfa8276e9c",
+    "url": "/ReactFirst-sCodeResume/index.html"
+  }
+];
